f5dadf45ed8a42d3567d797574b5f3e46523c839
nankopeter <nanko.peter@gmail.com>
2021-03-02 15:06:32 +0100

    first commit

---
+++ src/main/java/sk/timak/testprojekt/TestprojektApplication.java
@@ -0,0 +1,13 @@
+package sk.timak.testprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestprojektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestprojektApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sk/timak/testprojekt/TestprojektApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.timak.testprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestprojektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
