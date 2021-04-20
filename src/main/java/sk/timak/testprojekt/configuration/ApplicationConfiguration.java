bb3275584f3266fd5764f160c70a63d4b2acba24
nankopeter <nanko.peter@gmail.com>
2021-04-20 02:26:05 +0200

    app configuration

--- src/main/java/sk/timak/testprojekt/configuration/ApplicationConfiguration.java
+++ src/main/java/sk/timak/testprojekt/configuration/ApplicationConfiguration.java
@@ -2,22 +2,31 @@ package sk.timak.testprojekt.configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.Arrays;
+
+@Configuration
 @EnableSwagger2
 @EnableWebSecurity
-@Configuration
 public class ApplicationConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Value("${cross.origin.resource}")
+    private String allowedCrossOrigin;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -26,6 +35,19 @@ public class ApplicationConfiguration extends WebSecurityConfigurerAdapter {
                 .anyRequest()
                 .permitAll()
                 .and().csrf().disable();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        final CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH"));
+        configuration.addAllowedOrigin(allowedCrossOrigin);
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(Arrays.asList("Cache-Control", "Content-Type"));
+        configuration.setMaxAge(3600L); //half hour
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Bean
